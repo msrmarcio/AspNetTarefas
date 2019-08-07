b0e301a4abe6bdae37f93f350c2df3343a0e0103
Marcio R <marcioprogramer@gmail.com>
2019-08-07 08:20:40 -0300

    projeto

---
+++ Impacta.Tarefas/Impacta.MOD/Editora.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Impacta.MOD
+{
+    public class Editora
+	{
+        [Display(Name ="CÓDIGO DA EDITORA")]
+		public int EditoraId { get; set; }
+        [Display(Name ="RAZÃO SOCIAL")]
+        [Required(ErrorMessage ="Razão social deve ser informada")]
+		public string Nome { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage ="E-mail de contato não esta sendo informado")]
+		public string Email { get; set; }
+        [Required]
+        public string Cnpj { get; set; }
+        [Phone]
+        public string Telefone { get; set; }
+
+        public Endereco Endereco { get; set; }
+    }
+}
+
+/*
+ <div class="row">
+        <div class="form-group col-md-4">
+            <label for="campo1">Campo Um</label>
+            <input type="text" class="form-control" id="campo1">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label for="campo2">Campo Dois</label>
+            <input type="text" class="form-control" id="campo3">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label for="campo3">Campo Três</label>
+            <input type="text" class="form-control" id="campo3">
+        </div>
+    </div>
+    */
---
+++ Impacta.Tarefas/Impacta.Tarefas/Controllers/RealBooksController.cs
@@ -0,0 +1,105 @@
+﻿using System.Web.Mvc;
+using Impacta.MOD;
+using Impacta.Tarefas.Business;
+
+namespace Impacta.Tarefas.Controllers
+{
+	public class RealBooksController : Controller
+	{
+		// GET: RealBooks
+		public ActionResult Index()
+		{
+
+            
+            return View();
+            //// criar um objeto do tipo Business
+			//EditoraBUS editoraBUS = new EditoraBUS();
+
+			//var listaEditora = editoraBUS.ListarEditoras();
+
+			//// Retornamos a lista para a VIEW que estara tipada para 
+			//// receber uma lista de Editoras
+			//return View(listaEditora);
+		}
+
+		// GET: RealBooks/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: RealBooks/Create
+		public ActionResult CriarEditora()
+		{
+
+			return View();
+		}
+
+		// POST: RealBooks/Create
+		[HttpPost]
+		public ActionResult Create(Editora collection)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					
+				}
+				EditoraBUS editoraBUS = new EditoraBUS();
+
+				editoraBUS.Salvar(collection);
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				
+				return View();
+			}
+		}
+
+		// GET: RealBooks/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: RealBooks/Edit/5
+		[HttpPost]
+		public ActionResult Edit(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add update logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: RealBooks/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: RealBooks/Delete/5
+		[HttpPost]
+		public ActionResult Delete(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
